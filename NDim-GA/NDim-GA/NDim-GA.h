d788578b62ac60948def8d629395f64b60478bcd
Adam\Dell <adamhorvath9412@gmail.com>
2018-05-25 16:00:00 +0200

    Selection operator nearly ready

--- NDim-GA/NDim-GA/NDim-GA.h
+++ NDim-GA/NDim-GA/NDim-GA.h
@@ -8,16 +8,19 @@
 class NDim_GA 
 {
 public:
-	NDim_GA(int dimension, int populationSize, int generationSize);
+	NDim_GA(int dimension, int populationSize, int generationSize, int crossoverMax);
 private:
 	int dim;
 	int popsize;
 	int generation;
+	int crossover;
 
 	void simulate();
 	void populationInit(vector < Chromosome> &population);
 	vector<double> createNDimPoint();
 	double getFitness(vector<double> points);
-	void NDim_GA::wheelInit(vector<Chromosome> population, vector<Wheel> &w);
+	void wheelInit(vector<Chromosome> population, vector<Wheel> &w);
 	double randomFloatNM(double xmin, double xmax);
+	vector<Chromosome> selection(double rouletteminval, double roulettemaxval, vector<Chromosome> population, vector<Wheel> w);
+	Chromosome single_point(Chromosome parent1, Chromosome parent2);
 };
