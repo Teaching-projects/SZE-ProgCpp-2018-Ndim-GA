f5d5bd727ace791395e0c053f0c5463f705319d2
szabolcsfuzi <fuziszabolcsattila@gmail.com>
2018-05-29 16:06:43 +0200

    Added missing mutation variable

--- NDim-GA/NDim-GA/NDim-GA.h
+++ NDim-GA/NDim-GA/NDim-GA.h
@@ -5,16 +5,17 @@
 class NDim_GA : public EvoAlgs
 {
 public:
-	NDim_GA(int dimension, int populationSize, int generationSize, int crossoverId, int crossoverMax, int testFunctionId, double min, double max);
+	NDim_GA(int dimension, int populationSize, int generationSize, int crossoverId, int crossoverMax, int testFunctionId, double mutationMax, double min, double max);
 private:
 	int crossover;
 	int crossoverType;
+	double mutation;
 
 	void simulate();
 	void wheelInit(vector<Chromosome> population, vector<Wheel> &w);
 	vector<Chromosome> selection(double rouletteminval, double roulettemaxval, vector<Chromosome> population, vector<Wheel> w);
 	void selectingSurvivors(double rouletteminval, double roulettemaxval, vector<Chromosome> population, vector<Wheel> w, vector<Chromosome> &popbuff);
-	void mutation(vector<Chromosome> &popbuff);
+	void doMutation(vector<Chromosome> &popbuff);
 
 	Chromosome doCrossover(Chromosome parent1, Chromosome parent2);
 	Chromosome single_point(Chromosome parent1, Chromosome parent2);
